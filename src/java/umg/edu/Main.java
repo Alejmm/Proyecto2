252d0a2b5b2842da5c431eeeaf5c3b7eed2e0cfd
Alejandro <cristianmelgar1010@gmail.com>
2022-09-30 23:14:26 -0600

    Proyecto 2 agregando informacion personal

--- src/java/umg/edu/Main.java
+++ src/java/umg/edu/Main.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 
 import java.io.IOException;
-
+//Cristian Alejandro Melgar Ordoñez 7690 21 8342
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
